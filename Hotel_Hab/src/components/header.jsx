67c8c17f232874608976f6e32fb9e888bdac2b60
Jorgemg91 <jorge.moreno.gomez.torre@gmail.com>
2024-06-06 13:40:30 +0200

    mensaje header

--- Hotel_Hab/src/components/header.jsx
+++ Hotel_Hab/src/components/header.jsx
@@ -1,8 +1,20 @@
 import 'boxicons'
+import React, { useState } from 'react';
 import { Link } from "react-router-dom";
 
 
 function Header() {
+
+  const [showMessage, setShowMessage] = useState(false);
+
+  const handleMouseEnter = () => {
+      setShowMessage(true);
+  };
+
+  const handleMouseLeave = () => {
+      setShowMessage(false);
+  };
+
   return (
     <>
       <div className=' bg-slate-700 w-auto flex items-center relative'>
@@ -12,7 +24,17 @@ function Header() {
           <h2 className='text-1xl  text-amber-600 mt-2'>El primer hotel de Europa con Inteligencia Artificial</h2>
         </div>
         <div className="flex gap-4 absolute top-0 right-0 p-3 mr-1">
-        <box-icon name='envelope' color='#f3f2f2' ></box-icon>
+        <box-icon
+                name="envelope"
+                color="#f3f2f2"
+                onMouseEnter={handleMouseEnter}
+                onMouseLeave={handleMouseLeave}
+            ></box-icon>
+            {showMessage && (
+                <div className="absolute top-1 right-20 mt-2 w-64 p-2 bg-white border border-gray-300 rounded">
+                    <p>No tienes mensajes</p>
+                </div>
+            )}
          <Link to='/login'> <box-icon name='user' color='#ffffff' ></box-icon></Link> 
         </div>
         <div className='flex gap-5 text-gray-300 p-3 mr-4 absolute bottom-0 right-0 text-xl'>
