d07f2f51c6f37bb28f5f5a206a70a789af5eb381
Rodrigo <rodrigo.alonso.orozco@gmail.com>
2024-06-06 13:05:48 +0200

    cambio a Spanish

--- Hotel_Hab/src/components/login/login.jsx
+++ Hotel_Hab/src/components/login/login.jsx
@@ -27,7 +27,7 @@ const Login = () => {
                   htmlFor="password"
                   className="block mb-2 text-sm font-medium text-gray-900 dark:text-black"
                 >
-                  password
+                  Contraseña
                 </label>
                 <input
                   type="password"
@@ -50,14 +50,14 @@ const Login = () => {
                   htmlFor="remember"
                   className="ms-2 text-sm font-medium text-gray-900 dark:text-black"
                 >
-                  Remember me
+                  Recordar
                 </label>
               </div>
               <button
                 type="submit"
                 className="text-white bg-orange-300 hover:bg-orange-400 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-orange-400 dark:hover:bg-orange-700 dark:focus:ring-orange-800"
               >
-                Submit
+                Ingresar
               </button>
             </form>
           </div>
@@ -70,7 +70,7 @@ const Login = () => {
                   htmlFor="email"
                   className="block mb-2 text-sm font-medium text-gray-900 dark:text-black"
                 >
-                  Your email
+                  Tu email
                 </label>
                 <input
                   type="email"
@@ -85,7 +85,7 @@ const Login = () => {
                   htmlFor="password"
                   className="block mb-2 text-sm font-medium text-gray-900 dark:text-black"
                 >
-                  Your password
+                  Contraseña
                 </label>
                 <input
                   type="password"
@@ -99,7 +99,7 @@ const Login = () => {
                   htmlFor="repeat-password"
                   className="block mb-2 text-sm font-medium text-gray-900 dark:text-black"
                 >
-                  Repeat password
+                 Repetir Contraseña
                 </label>
                 <input
                   type="password"
@@ -122,12 +122,12 @@ const Login = () => {
                   htmlFor="terms"
                   className="ms-2 text-sm font-medium text-gray-900 dark:text-black"
                 >
-                  I agree with the{" "}
+                  Acepto Terminos y Condiciones{" "}
                   <a
                     href="#"
                     className="text-blue-600 hover:underline dark:text-black"
                   >
-                    terms and conditions
+                    
                   </a>
                 </label>
               </div>
@@ -135,7 +135,7 @@ const Login = () => {
                 type="submit"
                 className="text-white bg-orange-300 hover:bg-orange-400 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-orange-400 dark:hover:bg-orange-700 dark:focus:ring-orange-800"
               >
-                Register new account
+                Registrar Nueva Cuenta
               </button>
             </form>
           </div>
