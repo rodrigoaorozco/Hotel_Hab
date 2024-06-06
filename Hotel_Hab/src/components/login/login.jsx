e345a28eb6d9a66adddd19b9e35a157993031ecc
Rodrigo <rodrigo.alonso.orozco@gmail.com>
2024-06-06 10:33:03 +0200

    login corregido prueba

--- Hotel_Hab/src/components/login/login.jsx
+++ Hotel_Hab/src/components/login/login.jsx
@@ -1,48 +1,68 @@
 import React from "react";
 
-export const Login () => {
-    return (
-        <div className="contenedor">    
-        
-    <div id="loginbox" className="caja principal col-md-6 col-md-offset-3 col-sm-6 col-sm-offset-3">
-        
-        <div clase="fila">                
-            <div className="iconomelón">
-              <img src="images/lock.png" width="128" height="128" alt="Inicio de sesión">
-            </div>
+const Login = () => {
+  return (
+    <div className="contenedor">
+      <div
+        id="loginbox"
+        className="principal-box col-md-6 col-md-offset-3 col-sm-6 col-sm-offset-3"
+      >
+        <div className="row">
+          <div className="icon-box">
+            <img
+              src="images/lock.png"
+              width="128"
+              height="128"
+              alt="Inicio de sesión"
+            />
+          </div>
         </div>
-        
-        <div className="panel panel-predeterminado" >
-            <div className="encabezado del panel">
-                <div className="panel-title text-center">Inicio de sesión</div>
-            </div>     
 
-            <div className="cuerpo-panel" >
+        <div className="panel panel-default">
+          <div className="panel-heading">
+            <div className="panel-title text-center">Inicio de sesión</div>
+          </div>
 
-                <formulario id="formulario" clase="formulario-horizontal">
-                   
-                    <div className="grupo-entrada">
-                        <span className="input-group-addon"><i className="glifo glifo-usuario"></i></span>
-                        <input type="text" className="form-control" name="usuario" value="" placeholder="Número de empleado" autofocus="">                                        
-                    </div>
+          <div className="panel-body">
+            <form id="loginform" className="form-horizontal">
+              <div className="input-group">
+                <span className="input-group-addon">
+                  <i className="glyphicon glyphicon-user"></i>
+                </span>
+                <input
+                  type="text"
+                  className="form-control"
+                  name="usuario"
+                  placeholder="Número de empleado"
+                  autoFocus
+                />
+              </div>
 
-                    <div className="grupo-entrada">
-                        <span className="input-group-addon"><i className="glifo glifo-bloqueo"></i></span>
-                        <tipo de entrada="contraseña" clase="control-formulario" nombre="contrasenia" placeholder="Contraseña">
-                    </div>                                                                  
+              <div className="input-group">
+                <span className="input-group-addon">
+                  <i className="glyphicon glyphicon-lock"></i>
+                </span>
+                <input
+                  type="password"
+                  className="form-control"
+                  name="password"
+                  placeholder="Contraseña"
+                />
+              </div>
 
-                    <div className="grupo-formulario">
-                        <!-- Botón -->
-                        <div className="controles col-sm-12">
-                            <button type="submit" href="#" className="btn btn-primary pull-right"><i className="glyphicon glyphicon-log-in"></i> Entrar</button>                          
-                        </div>
-                    </div>
-
-                </formulario>     
-
-            </div>                     
-        </div>  
+              <div className="form-group">
+                <div className="controls col-sm-12">
+                  <button type="submit" className="btn btn-primary pull-right">
+                    <i className="glyphicon glyphicon-log-in"></i> Entrar
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
-</div>
-    )
-}
+  );
+};
+
+export default Login;
