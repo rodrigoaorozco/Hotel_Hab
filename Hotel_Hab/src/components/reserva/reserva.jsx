8fdc5e1c025691c11637ddf1313f9057ecfcc6b0
Jorgemg91 <jorge.moreno.gomez.torre@gmail.com>
2024-06-12 10:00:50 +0200

    arreglando el footer

--- Hotel_Hab/src/components/reserva/reserva.jsx
+++ Hotel_Hab/src/components/reserva/reserva.jsx
@@ -4,7 +4,11 @@ import React from "react";
  function Reserva () {
     return (
       <>
+       <div className="w-3/4 m-auto mt-6 text-center">
+       <h2 className=' text-xl font-bold text-orange-600'>RESERVA TU ESTANCIA</h2>
        
+
+       </div>
       </>
     );
 }
